c476500f4da88651aa0badc41f5b2248714fef4c
smitha s nair <smitha.sasidharannair@smail.rasmussen.edu>
2017-05-24 22:58:04 -0500

    Web app initial checkin

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// This is an example of config.php
+$dbhost = 'us-cdbr-azure-southcentral-f.cloudapp.net';
+$dbuser = 'bbece113092b36';
+$dbpass = 'd9e55a1e';
+$dbname = 'db-83e13e57-9556';
+?>
